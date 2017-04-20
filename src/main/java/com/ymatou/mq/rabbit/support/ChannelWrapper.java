91caa97acb8b9c83cd275156d48abd2cc96d5629
zhangzhihua <zhangzhihua@ymatou.com>
2017-04-20 17:28:48 +0800

    改unconfrimedmap

--- src/main/java/com/ymatou/mq/rabbit/support/ChannelWrapper.java
+++ src/main/java/com/ymatou/mq/rabbit/support/ChannelWrapper.java
@@ -1,17 +1,13 @@
 package com.ymatou.mq.rabbit.support;
 
 import com.rabbitmq.client.Channel;
-import com.rabbitmq.client.ConfirmListener;
 import com.rabbitmq.client.Recoverable;
 import com.rabbitmq.client.RecoveryListener;
 import com.rabbitmq.client.impl.recovery.AutorecoveringChannel;
-import com.ymatou.mq.rabbit.RabbitChannelFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.util.Collections;
 import java.util.SortedMap;
-import java.util.TreeMap;
 
 /**
  * channel wrapper，主要用于处理ack事件
@@ -29,7 +25,7 @@ public class ChannelWrapper {
     /**
      * 未确认集合
      */
-    private SortedMap<Long, Object> unconfirmedSet;
+    private SortedMap<Long, Object> unconfirmedMap;
 
     /**
      * 当前channel所在线程
@@ -52,8 +48,8 @@ public class ChannelWrapper {
                 @Override
                 public void handleRecovery(Recoverable recoverable) {
                     logger.warn("channel handleRecovery,recoverable:{}",recoverable);
-                    if(unconfirmedSet != null){
-                        unconfirmedSet.clear();
+                    if(unconfirmedMap != null){
+                        unconfirmedMap.clear();
                     }
                 }
 
@@ -65,9 +61,9 @@ public class ChannelWrapper {
         }
     }
 
-    public ChannelWrapper(Channel channel,SortedMap<Long, Object> unconfirmedSet){
+    public ChannelWrapper(Channel channel,SortedMap<Long, Object> unconfirmedMap){
         this.channel = channel;
-        this.unconfirmedSet = unconfirmedSet;
+        this.unconfirmedMap = unconfirmedMap;
     }
 
     public Channel getChannel() {
@@ -78,12 +74,12 @@ public class ChannelWrapper {
         this.channel = channel;
     }
 
-    public SortedMap<Long, Object> getUnconfirmedSet() {
-        return unconfirmedSet;
+    public SortedMap<Long, Object> getUnconfirmedMap() {
+        return unconfirmedMap;
     }
 
-    public void setUnconfirmedSet(SortedMap<Long, Object> unconfirmedSet) {
-        this.unconfirmedSet = unconfirmedSet;
+    public void setUnconfirmedMap(SortedMap<Long, Object> unconfirmedMap) {
+        this.unconfirmedMap = unconfirmedMap;
     }
 
     public Thread getThread() {
